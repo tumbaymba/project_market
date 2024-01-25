c0609180e785ac27b8cf56c28c2582ae28e728a7
Mary Melnikova <marymelnikova1988@gmail.com>
2024-01-25 23:24:45 +0300

    добавление 21/1 1 задание

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, home, product
+from catalog.views import contacts, ProductListView, HomeView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home),
+    # path('', home),
+    path('', HomeView.as_view()),
     path('contacts/', contacts),
-    path('home/', home),
-    path('<int:pk>/product', product, name='product'),
+    # path('home/', home),
+    path('home/', HomeView.as_view()),
+    # path('<int:pk>/product', product, name='product'),
+    path('<int:pk>/product/', ProductListView.as_view(), name='product'),
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, TemplateView
 
 from catalog.models import Product
 
@@ -15,16 +16,32 @@ def contacts(request):
         print(f"{name} ({phone}, {email}): {message}")
     return render(request, 'catalog/contacts.html')
 
-def home(request):
-    context = {
-        'object_list': Product.objects.all(),
-    }
-    return render(request, 'catalog/home.html', context)
+# def home(request):
+#     context = {
+#         'object_list': Product.objects.all(),
+#     }
+#     return render(request, 'catalog/home.html', context)
 
-def product(request, pk):
-    product_item = Product.objects.get(pk=pk)
-    context = {
-        'object_list': Product.objects.filter(id=pk),
-    }
-    return render(request, 'catalog/product.html', context)
+class HomeView(TemplateView):
+    template_name = 'catalog/home.html'
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data ['object_list'] = Product.objects.all()
+        return context_data
+
+# def product(request, pk):
+#     product_item = Product.objects.get(pk=pk)
+#     context = {
+#         'object_list': Product.objects.filter(id=pk),
+#     }
+#     return render(request, 'catalog/product_list.html', context)
+
+class ProductListView(ListView):
+    model = Product
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(id=self.kwargs.get('pk'))
+        return queryset
+        
