461a804a7567dad39a5e11a1143ba8f135e4cf87
Mary Melnikova <marymelnikova1988@gmail.com>
2024-01-28 14:04:21 +0300

    добавление 22/1 1 задание с валидацией

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,13 +2,35 @@ from django import forms
 
 from catalog.models import Product
 
+
 class StyleFormMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
-class ProductForm(StyleFormMixin,forms.ModelForm):
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    def clean_title(self):
+        cleaned_data = self.cleaned_data['title'].lower()
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                           'радар']
+        for word in forbidden_words:
+            if word in cleaned_data:
+                raise forms.ValidationError('Нельзя в названии продукта употреблять запрещенные слова')
+
+        return cleaned_data
+    def clean_description(self):
+        cleaned_data = self.cleaned_data['description'].lower()
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                           'радар']
+        for word in forbidden_words:
+            if word in cleaned_data:
+                raise forms.ValidationError('Нельзя в описании продукта употреблять запрещенные слова')
+
+        return cleaned_data
 
