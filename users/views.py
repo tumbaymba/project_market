cdf5b772671813d5b762fa9adcfffd690591c716
Mary Melnikova <marymelnikova1988@gmail.com>
2024-01-30 15:48:38 +0300

    22/2 регистрацию пользователя по почте и паролю;

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import UserRegisterForm
+from users.models import User
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
