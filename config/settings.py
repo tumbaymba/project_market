d5210dffbbac126cded13564b5a7595e92ecb719
Mary Melnikova <marymelnikova1988@gmail.com>
2024-01-30 15:33:42 +0300

    22/2 авторизация

--- config/settings.py
+++ config/settings.py
@@ -137,3 +137,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.USER'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
