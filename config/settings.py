80a9689741932cd330d73017812cef4154ace967
Dmitry Pavlushin <pavlushindv@gmail.com>
2024-06-06 15:43:19 +0300

    Correct DZ from 06.06.2024 update .gitignore, settings.py, create .env

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ load_dotenv(dotenv_path=dot_env)
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=!_^n$al=smvicpq_#+=_d2x+50j)033cdgk&l&tei8l*m3u@8'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
